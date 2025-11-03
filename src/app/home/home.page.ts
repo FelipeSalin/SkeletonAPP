18c63fca58c5f8fa4abb0e91f82299932d57149c
FelipeSalin <fea.salinas@duocuc.cl>
2025-11-03 15:58:00 -0300

    Intento de enrutador con páginas Home y Login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+/** Importaciones de librerías a usar */
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// Decorador Componente este indica que el Home Page es un Componente
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-home', // Nombre del selector como <input> o <main-page>
+  templateUrl: 'home.page.html', // Archivo HTML de la visual a trabajar
+  styleUrls: ['home.page.scss'], // Archivo/s de estilos
   standalone: false,
 })
 export class HomePage {
+  data: any; // Generamos una variable Any (permite cualquier valor)
 
-  constructor() {}
-
+  /**
+   * En el constructor del HomePage se colocan por parametros
+   * todas aquellas propiedades con el siguiente formato
+   * private = visibilidad
+   * activeRoute = identificador
+   * ActiveRoute = Tipo de objeto
+   * : indica que el identificador sera de la clase posterior a los : puntos
+   * 
+   */
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    // Se llama a la ruta activa y se obtiene sus parametros mediante una suscripcion
+    this.activeroute.queryParams.subscribe(params => { // Utilizamos lambda
+      if (this.router.getCurrentNavigation()?.extras.state) { // Validamos que en la navegacion actual tenga extras
+        this.data = this.router.getCurrentNavigation()!.extras.state!['user']; // Si tiene extra rescata lo enviado
+        console.log(this.data) // Muestra por consola lo traido
+        }else{this.router.navigate(["/login"])} // Si no tiene extra la navegacion actual navegar al login
+    });
+  }
 }
+
+
+//@Component({
+  //selector: 'app-pipes',
+  //template: '<p>{{hoy}}</p>',
+  //styleUrls: ['./pipes.component.css']
+//})
+//export class PipesComponent {
+
+  //hoy = new Date();
+  //constructor() { }
+//}
+
+//<p>{{hoy | date}}</p>
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  /**
+   * Se genera el modelo user con dos claves
+   * cada clave tiene su valor inicial
+   */
+  user={
+    usuario:"",
+    password:""
+  }
+  constructor(private router: Router) { } // Se debe instanciar
 
   ngOnInit() {
   }
-
+  ingresar(){
+    // Se declara e instancia un elemento de tipo NavigationExtras
+    let navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user // Al estado se asignamos un objeto con clave y valor
+      }
+    };
+    this.router.navigate(['/home'],navigationExtras); // navegamos hacia el Home y enviamos información adicional
+  }
 }
