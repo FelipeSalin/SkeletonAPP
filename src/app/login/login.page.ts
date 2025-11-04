416dace19697ebbf95594be76650159e0f9f3d82
FelipeSalin <fea.salinas@duocuc.cl>
2025-11-03 22:54:53 -0300

    Validaciones de usuario y contraseña incorporadas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,52 @@ export class LoginPage {
     usuario:"",
     password:""
   }
-  constructor(private router: Router) { } // Se debe instanciar
+  constructor(private router: Router, private alertController: AlertController) { } // Se debe instanciar
+
+  // Método para mostrar alerta de error
+  async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  //Función para validar el formato usuario
+  validarEmail(usuario: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expresión regular básica para validar email
+    return emailRegex.test(usuario);
+  }
+
+  login() {
+    // Verificar que el campo de correo no esté vacío
+    if (!this.user.usuario) {
+      this.mostrarAlerta('El campo de usuario no puede estar vacío.');
+      return;
+    }
+
+    // Validar el formato del usuario
+    if (this.user.usuario.length < 3 || this.user.usuario.length > 8) {
+      this.mostrarAlerta('El usuario debe tener mínimo 3 y máximo 8 caracteres.');
+      return;
+    }
+
+    // Verificar que el campo de correo no esté vacío
+    if (!this.user.password) {
+      this.mostrarAlerta('El campo de contraseña no puede estar vacía.');
+      return;
+    }
+
+    // Verificar que la contraseña tenga 4 caracteres
+    if (this.user.password.length !== 4) {
+      this.mostrarAlerta('La constraseña debe ser de 4 caracteres.');
+      return;
+    }
+
+    // Si todas las validaciones son correctas, navega a la página "home"
+    this.router.navigate(['/home'], { state: { user: this.user.usuario } });
+  }
 
   ingresar(){
     // Se declara e instancia un elemento de tipo NavigationExtras
