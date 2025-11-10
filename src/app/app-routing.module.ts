1681ab690333c4c18565d3cba93f7a61e1b32e4a
FelipeSalin <fea.salinas@duocuc.cl>
2025-11-10 12:07:27 -0300

    Home agregado y página de registro inicializada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
 ];
 
 @NgModule({
